c0be61513da93e7c5e6f3ec95fd5ce10efc9ec77
William (Billy) Noyes <wbillypn@gmail.com>
2020-09-11 22:40:48 +0100

    Implemented components and created sticky bottom overlay

--- src/App.js
+++ src/App.js
@@ -1,11 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import Header from "./Sections/Header/Header";
+import Project from "./Sections/Project/Project";
+import { Paper, Switch } from "@material-ui/core";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const theme = createMuiTheme({
+    palette: {
+      type: darkMode ? "dark" : "light",
+    },
+  });
+
+  const handleSwitch = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
-    <div className="app">
-      <h1>Hello World</h1>
-    </div>
+    <ThemeProvider theme={theme} className="app">
+      <Paper elevation={0} style={{ height: "100%" }}>
+        <div className="app__topBar">
+          <h4>billynoyes</h4>
+          <Switch
+            checked={darkMode}
+            onChange={handleSwitch}
+            color="default"
+            className="app__switch"
+          />
+        </div>
+        <Header />
+        <Project
+          projectScreenshot="https://media.idownloadblog.com/wp-content/uploads/2016/07/macOS-Sierra-Picture-in-Picture-enable-on-YouTube-Mac-screenshot-001.jpg"
+          projectName="Slack Clone"
+          projectStack="React, MaterialUI, Firebase Firestore & Authentication"
+          projectDescription="Clone of slack with a fully functioning real-time chat for each channel and login system implemented using the Firebase Authentication to give a user their own account to chat in each of the channels."
+          projectLink="https://slack-clone-17944.web.app/"
+        />
+        <Project
+          projectScreenshot="https://media.idownloadblog.com/wp-content/uploads/2016/07/macOS-Sierra-Picture-in-Picture-Vimeo-Mac-screenshot-001.jpg"
+          projectName="Slack Clone"
+          projectStack="React, MaterialUI, Firebase Firestore & Authentication"
+          projectDescription="Clone of slack with a fully functioning real-time chat for each channel and login system implemented using the Firebase Authentication to give a user their own account to chat in each of the channels."
+          projectLink="https://slack-clone-17944.web.app/"
+        />
+
+        <div className={`app__overlay ${darkMode ? "app__overlayDark" : ""}`}>
+          <h2>
+            <a href="mailto:wbillypn@gmail.com">wbillypn@gmail.com</a> ───{" "}
+            <a href="http://instagram.com/billy.noyes">Instagram</a> ───{" "}
+            <a href="https://github.com/BillyNoyes">GitHub</a> ───{" "}
+            <a href="https://www.linkedin.com/in/billynoyes/">LinkedIn</a>
+          </h2>
+        </div>
+      </Paper>
+    </ThemeProvider>
   );
 }
 
